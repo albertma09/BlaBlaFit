68cb62c71440b836e71944c816880041bf942407
Carlos Jimenez <jimenez.martin.carlos@alumnat.copernic.cat>
2022-10-15 01:39:49 +0200

    Afegir activity de registre , WIP

---
+++ app/src/main/java/com/example/blablafit/Enviar_Contrasenya.kt
@@ -0,0 +1,11 @@
+package com.example.blablafit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Enviar_Contrasenya : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_enviar_contrasenya)
+    }
+}
---
+++ app/src/main/java/com/example/blablafit/Recupera_Contrasenya.kt
@@ -0,0 +1,11 @@
+package com.example.blablafit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Recupera_Contrasenya : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recupera_contrasenya)
+    }
+}
