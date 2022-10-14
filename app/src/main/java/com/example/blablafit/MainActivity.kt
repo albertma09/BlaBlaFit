eae006090d5f4cc1cf5a4510ee3e0c275e9f488e
carlos <carlos_007_34@outlook.com>
2022-10-14 20:00:22 +0200

    Afegir finestra principal amb splashscreen

--- app/src/main/java/com/example/blablafit/MainActivity.kt
+++ app/src/main/java/com/example/blablafit/MainActivity.kt
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.Theme_Blablafit)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
