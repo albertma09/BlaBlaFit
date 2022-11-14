80cbe65f512b5650f42fff3e7f385bc9bc674018
Carlos Jimenez <jimenez.martin.carlos@alumnat.copernic.cat>
2022-11-14 19:45:19 +0100

    traer merge de Albert

--- app/src/main/java/com/example/blablafit/MainApp.kt
+++ app/src/main/java/com/example/blablafit/MainApp.kt
@@ -1,16 +1,18 @@
 package com.example.blablafit
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentManager
 import com.example.blablafit.databinding.ActivityMainAppBinding
 
 class MainApp : AppCompatActivity() {
     lateinit var binding : ActivityMainAppBinding
     lateinit var toggle: ActionBarDrawerToggle
-
+    val fragmentManager = supportFragmentManager
+    val transaction = fragmentManager.beginTransaction()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainAppBinding.inflate(layoutInflater)
