67006a043a0b6887ac3fb0cc236cc23d074b727c
Carlos Jimenez <jimenez.martin.carlos@alumnat.copernic.cat>
2022-11-22 19:09:55 +0100

    merge de develop a carlos

--- app/src/main/java/com/example/blablafit/fragmentsApp/DadesPersonals.kt
+++ app/src/main/java/com/example/blablafit/fragmentsApp/DadesPersonals.kt
@@ -1,16 +1,16 @@
 package com.example.blablafit.fragmentsApp
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
 import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.blablafit.R
-import com.example.blablafit.activities.MainApp
 import com.example.blablafit.databinding.FragmentDadesPersonalsBinding
 
-// TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
@@ -24,48 +24,40 @@ class DadesPersonals : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
-    private lateinit var bin : FragmentDadesPersonalsBinding
+    private lateinit var _binding: FragmentDadesPersonalsBinding
+    private val binding get() = _binding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
-        bin = FragmentDadesPersonalsBinding.inflate(layoutInflater)
-
-
-
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        _binding = FragmentDadesPersonalsBinding.inflate(layoutInflater)
+        val view = binding.root
+        _binding.agua.setOnClickListener { updateBar() }
+        return view
         // Inflate the layout for this fragment
+        //return inflater.inflate(R.layout.fragment_dades_personals, container, false)
+    }
 
-        return inflater.inflate(R.layout.fragment_dades_personals, container, false)
+    fun updateBar(){
 
-
+       /* val i : ProgressBar = findViewById(R.id.indicador)
+        progres = i.progress+10
+        i.progress= progres
+        Toast.makeText(this,progres.toString(), Toast.LENGTH_LONG).show()
+        if(i.progress == i.max){
+            i.progress =0
+            Toast.makeText(this,"Has bebido 2 litros", Toast.LENGTH_LONG).show()
+        }*/
 
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment DadesPersonals.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            DadesPersonals().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+
 }
